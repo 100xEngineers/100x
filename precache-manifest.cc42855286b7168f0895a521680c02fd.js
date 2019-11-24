67a2a0457530c8aee0d10a1046feb2dbde4adc14
Kaushik <kaushik.makwana@truebil.com>
2019-11-24 19:27:28 +0530

    Updates

---
+++ precache-manifest.cc42855286b7168f0895a521680c02fd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d877a5a5c8a27ca6d74bec5074604a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b260e314e4d659904411",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "e3bf8ca0244c82dbeed2",
+    "url": "/static/css/main.7c7e8865.chunk.css"
+  },
+  {
+    "revision": "b260e314e4d659904411",
+    "url": "/static/js/2.95816cd7.chunk.js"
+  },
+  {
+    "revision": "e3bf8ca0244c82dbeed2",
+    "url": "/static/js/main.ecfe1f85.chunk.js"
+  },
+  {
+    "revision": "c184f70356d06bef6086",
+    "url": "/static/js/runtime-main.4842b89b.js"
+  },
+  {
+    "revision": "8547ceae0d543a17c0ca6f55ff13e606",
+    "url": "/static/media/boy.8547ceae.svg"
+  },
+  {
+    "revision": "ea7ecf20db996ff05063eef4c8c2d824",
+    "url": "/static/media/featured-astro.ea7ecf20.jpg"
+  },
+  {
+    "revision": "3fad8bb64a2e6e9718fb8c7595efc0bf",
+    "url": "/static/media/featured-cs.3fad8bb6.jpg"
+  },
+  {
+    "revision": "59417d07b1179da399dec42a9fe9194a",
+    "url": "/static/media/featured-politics.59417d07.jpg"
+  },
+  {
+    "revision": "10ce6f2a695efab78d60505b81af38f2",
+    "url": "/static/media/featured-travel.10ce6f2a.jpg"
+  },
+  {
+    "revision": "038dbd32d31ac61f875c7a8abf453e6c",
+    "url": "/static/media/female_avtar.038dbd32.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "dabcdaa294004a1e59704d4837497446",
+    "url": "/static/media/male_avatar.dabcdaa2.svg"
+  },
+  {
+    "revision": "4d21db34508be67eb2e5e13558427d09",
+    "url": "/static/media/men.4d21db34.jpg"
+  },
+  {
+    "revision": "c9cfd61d2b9a91898e933414bd605a07",
+    "url": "/static/media/notification.c9cfd61d.png"
+  },
+  {
+    "revision": "88688cf0265e297262d0cf7470c1609c",
+    "url": "/static/media/starl1.88688cf0.svg"
+  },
+  {
+    "revision": "4893a5546c0277993681f06c15f51c80",
+    "url": "/static/media/starl2.4893a554.svg"
+  },
+  {
+    "revision": "9bbb4763e8e984a2b9aef23a00892e5f",
+    "url": "/static/media/starl3.9bbb4763.svg"
+  },
+  {
+    "revision": "108cd994386db4b0151cbfe4f1867f01",
+    "url": "/static/media/women.108cd994.jpg"
+  },
+  {
+    "revision": "d660a6f12f91a8cbb56cda9a5f5d47d0",
+    "url": "/static/media/women2.d660a6f1.jpg"
+  },
+  {
+    "revision": "f64b2abd401fe9067595ff069b90088e",
+    "url": "/static/media/women3.f64b2abd.jpg"
+  },
+  {
+    "revision": "2edc375d1c442fab7cc2a5b3a246754b",
+    "url": "/static/media/women4.2edc375d.jpg"
+  }
+]);
